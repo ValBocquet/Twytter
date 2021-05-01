9ddd23fe546c4acab344e6ff2deb8130500e4c54
Valentin B <valentin.bocquet9@gmail.com>
2021-05-01 13:15:10 +0200

    verification form and generate token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,14 @@ class UsersController < ApplicationController
     end
 
     def create
+        user_params = params.require(:user).permit(:username, :email, :password, :password_confirmation)
+        @user = User.new(user_params)
+        if @user.valid?
+            render 'new'
+        else 
+            render 'new'
 
+        end
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+
+    has_secure_password
+    has_secure_token :confirmation_token
+
+    validates :username, 
+        format: {with: /\A[a-zA-Z0-9_]{3,20}\z/, message: '20 caractères maximum'},
+        uniqueness: {case_sensitive: false}
+
+    validates :email,
+        format: {with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/},
+        uniqueness: {case_sensitive: false}
+
+
 end
